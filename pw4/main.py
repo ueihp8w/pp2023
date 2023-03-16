3d8134efc88a74c41fc5795af4831143391ce261
ueihp8w <nguyenquangvinh.p8w@gmail.com>
2023-03-16 14:22:21 +0700

    Labwork 4

---
+++ pw4/main.py
@@ -0,0 +1,35 @@
+from input import StudentMarkSystem
+
+if __name__=="__main__":
+    marksheet = StudentMarkSystem()
+
+    while True:
+        print("\nStudent Mark Management System\n")
+        print("1. Input number of students and their information")
+        print("2. Input number of courses and their information")
+        print("3. Select a course to input marks for students")
+        print("4. List all courses")
+        print("5. List all students")
+        print("6. Show marks for a given course")
+        print("7. Show average GPA of student")
+        print("8. Exit\n")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            marksheet.input_students()
+        elif choice == 2:
+            marksheet.input_courses()
+        elif choice == 3:
+            marksheet.input_marks()
+        elif choice == 4:
+            marksheet.list_courses()
+        elif choice == 5:
+            marksheet.list_students()
+        elif choice == 6:
+            marksheet.show_marks()
+        elif choice == 7:
+            marksheet.show_GPAs()
+        elif choice == 8:
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
